d2e31ad4a3485314145af2235d00ada53ba0f683
Roman Abaghayn <84144854+777ARM777@users.noreply.github.com>
2024-03-10 00:31:16 +0400

    Update Node.h

--- Data Structures/C++/Binary Tree/Node.h
+++ Data Structures/C++/Binary Tree/Node.h
@@ -1,1 +1,19 @@
+#ifndef DATA_STRUCTURE_C_NODE_H
+#define DATA_STRUCTURE_C_NODE_H
 
+#endif //DATA_STRUCTURE_C_NODE_H
+
+template <typename T>
+struct Node
+{
+    Node(const T& d, Node<T>* l = nullptr, Node<T>* r = nullptr)
+            : data(d)
+            , left(l)
+            , right(r)
+    {
+    }
+
+    T data;
+    Node<T>* left;
+    Node<T>* right;
+};
